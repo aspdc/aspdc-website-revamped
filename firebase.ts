f47085e20e28ecb8e4fff162c35333727cdb2d50
NotTheRightGuy <janmejaychatterjee@gmail.com>
2024-08-21 14:03:45 +0530

    chore: Remove unused Firebase analytics import

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
