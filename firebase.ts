cbb33c68982bd64b4e371b863e1a4d447239b148
NotTheRightGuy <janmejaychatterjee@gmail.com>
2024-08-21 13:58:32 +0530

    Removed Firebase analytics

--- firebase.ts
+++ firebase.ts
@@ -22,7 +22,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-export { db, analytics };
+export { db };
