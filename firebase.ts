6e89a4db5a7c6aa91da3e62b302e8b4a3a82f2b8
NotTheRightGuy <janmejaychatterjee@gmail.com>
2024-08-21 13:56:14 +0530

    Changed Firebase config

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,5 +22,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
-export { db };
+
+export { db, analytics };
